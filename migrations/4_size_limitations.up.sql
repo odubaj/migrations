c4980a958295a5f7db4e9fcebeeb9c809100e919
pavel_bortnik <pavel_bortnik@epam.com>
2019-11-29 18:36:56 +0300

    added materialized view for projects' info

--- migrations/4_size_limitations.up.sql
+++ migrations/4_size_limitations.up.sql
@@ -23,3 +23,23 @@ CREATE INDEX IF NOT EXISTS activity_creation_date_idx
 DROP INDEX IF EXISTS activity_object_idx;
 CREATE INDEX IF NOT EXISTS activity_object_idx
     ON activity (object_id);
+
+CREATE MATERIALIZED VIEW project_info AS
+(
+SELECT count(DISTINCT public.project_user.user_id)       AS usersquantity,
+       count(DISTINCT CASE
+                          WHEN (public.launch.mode = 'DEFAULT'::public."launch_mode_enum" AND
+                                public.launch.status <> 'IN_PROGRESS'::public."status_enum")
+                              THEN public.launch.id END) AS launchesquantity,
+       max(public.launch.start_time)                     AS lastrun,
+       public.project.id,
+       public.project.creation_date,
+       public.project.name,
+       public.project.project_type,
+       public.project.organization,
+       now()                                             AS last_refresh
+FROM public.project
+         LEFT OUTER JOIN public.launch ON public.project.id = public.launch.project_id
+         LEFT OUTER JOIN public.project_user ON public.project.id = public.project_user.project_id
+GROUP BY public.project.id, public.project.creation_date, public.project.name, public.project.project_type, public.project.organization
+    );
